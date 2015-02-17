e7ebf8012a86b96c922c940e42618affb7fab09c
jonhowell <j.a.howell@mmu.ac.uk>
2015-02-17 14:13:24 +0000

    Initial Commit

---
+++ ArchPort/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ArchPort
+//
+//  Created by Jon Howell on 17/02/2015.
+//  Copyright (c) 2015 Manchester Metropolitan University - ESS - essmobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ArchPort/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ArchPort
+//
+//  Created by Jon Howell on 17/02/2015.
+//  Copyright (c) 2015 Manchester Metropolitan University - ESS - essmobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
